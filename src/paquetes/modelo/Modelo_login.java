df6ca8c56e952059eb88a657157d26dff958c453
unknown <marlon.arteaga.u@gmail.com>
2025-02-17 21:28:23 -0500

    modificando el arbol de directorio, todo funciona ok

--- src/paquetes/modelo/Modelo_login.java
+++ src/paquetes/modelo/Modelo_login.java
@@ -4,7 +4,7 @@ public class Modelo_login {
     private Usuario[] usuarios;
 
     public Modelo_login() {
-        // Inicializar con algunos usuarios de prueba
+
         usuarios = new Usuario[] {
                 new Usuario("as_admin", "0123", "ASISTENTE"),
                 new Usuario("as_vuelo", "4567", "ADMINISTRADOR")
--- src/paquetes/vista/Vista_login.java
+++ src/paquetes/vista/Vista_login.java
@@ -4,17 +4,14 @@ import paquetes.contorlador.Controlador_login;
 
 public class Vista_login {
     public Vista_login(Controlador_login controlador_login) {
-  
+
     }
 
     public void MostrarLogin() {
-            System.out.println("+-----------------------------------+");
-            System.out.println("|          Iniciar Sesión           |");        
-            System.out.println("+-----------------------------------+");
-            System.out.println("+[Enter] para ingresar              +");
-         
-        
-    
+        System.out.println("+-----------------------------------+");
+        System.out.println("|          Iniciar Sesión           |");
+        System.out.println("+-----------------------------------+");
+        System.out.println("+[Enter] para ingresar              +");
     }
 
     // Muestra un mensaje de éxito
@@ -24,20 +21,16 @@ public class Vista_login {
         System.out.println("+-----------------------------------+");
         System.out.println("| ¡Bienvenido, " + usuario + "! ");
         System.out.println("| Rol: " + rol + "                ");
-        System.out.println("+-----------------------------------+" + //
-                        "");
-
+        System.out.println("+-----------------------------------");
     }
 
     public void MostrarError() {
-
         System.out.println("\n+-----------------------------------+");
         System.out.println("|          Error de Acceso          |");
         System.out.println("+-----------------------------------+");
         System.out.println("| Credenciales incorrectas.         |");
         System.out.println("| Por favor, inténtelo de nuevo.    |");
         System.out.println("+-----------------------------------+\n");
-
     }
 
 }
